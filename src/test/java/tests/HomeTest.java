b002b6e66e36dd616eea05c4df129faa33c2ba40
internship02 <zaklinabalevic1997@gmail.com>
2023-12-14 09:38:49 +0100

    Bottom Navigation Test Changes

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -20,34 +20,27 @@ public class HomeTest extends BaseTest {
         homePage = new HomePage();
 
         WebElement homeScreenTittle = helpers.waitForElementByXpath(homePageLocators.getHomeScreenTittle());
-        assertNotNull(homeScreenTittle, "Home screen tittle is null!");
-        assertEquals(homeScreenTittle.getText(), homePage.getHomeScreenTitleText());
+        assertEquals(homeScreenTittle.getText(), homePage.getHomeScreenTitleText(), "The home screen title text does not match!");
 
         WebElement expenseBtn = helpers.waitForElementByAccessibilityId(homePageLocators.getExpenseBtn());
-        assertNotNull(expenseBtn, "Expense button is null!");
         expenseBtn.click();
         assertTrue(expenseBtn.isSelected(), "Expense button is not selected!");
 
         WebElement expenseText = helpers.waitForElementByXpath(homePageLocators.getExpenseText());
-        assertNotNull(expenseText, "Expense Text is null!");
-        assertEquals(expenseText.getText(), homePage.getExpenseBtnText());
+        assertEquals(expenseText.getText(), homePage.getExpenseBtnText(), "The expense button text does not match!");
 
         WebElement expenseIcon = helpers.waitForElementByXpath(homePageLocators.getExpenseIcon());
-        assertNotNull(expenseIcon, "Expense icon is null!");
         assertTrue(expenseIcon.isDisplayed(), "Expense icon is not displayed");
 
 
         WebElement homeBtn = helpers.waitForElementByAccessibilityId(homePageLocators.getHomeBtn());
-        assertNotNull(homeBtn, "Home button is null!");
         homeBtn.click();
         assertTrue(homeBtn.isSelected(), "Home button is not selected!");
 
         WebElement homeText = helpers.waitForElementByXpath(homePageLocators.getHomeText());
-        assertNotNull(homeText, "Home Text is null!");
-        assertEquals(homeText.getText(), homePage.getHomeBtnText());
+        assertEquals(homeText.getText(), homePage.getHomeBtnText(), "The expense button text does not match!");
 
         WebElement homeIcon = helpers.waitForElementByXpath(homePageLocators.getHomeIcon());
-        assertNotNull(homeIcon, "Home icon is null!");
         assertTrue(homeIcon.isDisplayed(), "Home icon is not displayed!");
 
     }
